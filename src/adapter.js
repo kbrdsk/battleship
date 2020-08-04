b27dcb00d4aa776e558978c85e2f360497bbd966
Kabirdas Henry <kbrdsk@gmail.com>
2020-08-04 15:44:57 -0700

    created mock for battleship, some initial tests

--- src/adapter.js
+++ src/adapter.js
@@ -1,7 +1,60 @@
-const {startGame, gameOver} = require("./battleship.js");
-const {updateView} = require("./view.js");
+const {
+	startGame,
+	initializePlayer,
+	placeShips,
+	turn,
+	gameOver,
+} = require("./battleship.js");
+const { updateView } = require("./view.js");
 
-const testMock = () => updateView();
-const activeController = {};
+let gamePhase = "newGame";
+let activeBoard = null;
+let playerNameInput = null;
+let playerTypeSelection = null;
+let nextButton = null;
 
-module.exports = { activeController, testMock }
+const newGameButton = document.createElement("button");
+newGameButton.addEventListener("click", () => {
+	const game = {};
+	startGame(game);
+	gamePhase = "playerCreation";
+	playerNameInput = createNameInput();
+	playerTypeSelection = createTypeSelection();
+})
+
+function createNameInput(){
+	return document.createElement("input");
+}
+
+function createTypeSelection(){
+	return [];
+}
+
+function reset() {
+	gamePhase = "newGame";
+	activeBoard = null;
+	playerNameInput = null;
+	playerTypeSelection = null;
+}
+
+module.exports = {
+	get newGameButton() {
+		return newGameButton;
+	},
+	get activeBoard() {
+		return activeBoard;
+	},
+	get phase() {
+		return gamePhase;
+	},
+	get playerNameInput() {
+		return playerNameInput;
+	},
+	get playerTypeSelection() {
+		return playerTypeSelection;
+	},
+	get nextButton() {
+		return nextButton;
+	},
+	reset,
+};
--- src/adapter.test.js
+++ src/adapter.test.js
@@ -1,7 +1,51 @@
-const { testMock } = require("./adapter.js");
+const adapter = require("./adapter.js");
 
-jest.mock("./view.js", () => {return { updateView: () => {}}});
+jest.mock("./view.js", () => {
+	return { updateView: () => {} };
+});
+jest.mock("./battleship.js", () => {
+	return {
+		startGame: jest.fn(),
+		initializePlayer: jest.fn(),
+		placeShips: jest.fn(),
+		turn: jest.fn(),
+		gameOver: jest.fn(),
+	};
+});
 
-test("mocks updateView", () => {
-	expect(() => testMock()).not.toThrow();
+const battleship = require("./battleship.js");
+
+test("clicking new game button calls startGame with blank object", () => {
+	let newGameButton = adapter.newGameButton;
+	newGameButton.click();
+	expect(battleship.startGame.mock.calls[0][0]).toEqual({});
+	battleship.startGame.mockReset();
+	adapter.reset();
+})
+
+test("clicking new game button sets phase to player creation", () => {
+	let newGameButton = adapter.newGameButton;
+	newGameButton.click();
+	expect(adapter.phase).toBe("playerCreation");
+	adapter.reset();
+});
+
+test("clicking new game button initializes name input & type select", () => {
+	let newGameButton = adapter.newGameButton;
+	newGameButton.click();
+	expect(adapter.playerNameInput).not.toBe(null);
+	expect(adapter.playerTypeSelection).not.toBe(null);
+	adapter.reset();
+});
+
+test("resets after clicking newGameButton", () => {
+	let newGameButton = adapter.newGameButton;
+	newGameButton.click();
+	adapter.reset();
+	expect(adapter.newGameButton).not.toBe(null);
+	expect(adapter.activeBoard).toBe(null);
+	expect(adapter.phase).toBe("newGame");
+	expect(adapter.playerNameInput).toBe(null);
+	expect(adapter.playerTypeSelection).toBe(null);
+	expect(adapter.nextButton).toBe(null);
 });
