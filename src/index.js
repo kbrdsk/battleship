a084b4922a40f201d9ec024dc85db497cfb11879
Kabirdas Henry <kbrdsk@gmail.com>
2020-08-10 16:56:12 -0700

    changed npm test command to run all tests and show coverage, started writing view module

---
+++ src/index.js
@@ -0,0 +1,2 @@
+const updateView = require("./view.js");
+updateView();
--- src/view.js
+++ src/view.js
@@ -1,7 +1,50 @@
-const { activeController } = require("./adapter.js");
+const adapter = require("./adapter.js");
 
-function updateView() {
-	throw "Did not mock properly";
+const newGameButton = (() => {
+	const button = adapter.newGameButton;
+	button.textContent = "New Game";
+	button.addEventListener("click", updateView);
+	return button;
+})();
+
+const nextButton = () => {
+	const button = adapter.nextButton;
+	button.addEventListener("click", updateView);
+	button.textCotnent = "Next";
+	return button;
 }
 
-module.exports = { updateView };
+const draw = {
+	newGame: drawNewGame,
+	playerCreation: drawPlayerCreation,
+	shipPlacement: drawShipPlacement,
+	turn: drawTurn,
+	gameOver: drawGameOver,
+}
+
+function updateView() {
+	document.body.innerHTML = "";
+	draw[adapter.phase]();
+	console.log(adapter.phase);
+}
+
+function drawNewGame(){
+	document.body.appendChild(newGameButton);
+}
+
+function drawPlayerCreation(){
+	document.body.appendChild(adapter.playerNameInput);
+	for(let selection of adapter.playerTypeSelection){
+		document.body.appendChild(selection);
+	}
+	document.body.appendChild(nextButton());
+}
+
+function drawShipPlacement(){}
+
+function drawTurn(){}
+
+function drawGameOver(){}
+
+
+module.exports = updateView;
